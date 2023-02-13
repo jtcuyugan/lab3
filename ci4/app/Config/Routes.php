f8088b985c5ad19f2f43c005562a9ba6668d3a6a
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-13 12:22:19 +0800

    Update Routes.php

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -3,20 +3,20 @@
 namespace Config;
 
 // Create a new instance of our RouteCollection class.
-//$routes = Services::routes();
+$routes = Services::routes();
 
 /*
  * --------------------------------------------------------------------
  * Router Setup
  * --------------------------------------------------------------------
  */
-/*
+
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-*/
+
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -51,8 +51,7 @@ $routes->get('(:segment)', [Pages::class, 'view']);
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-/*
+
  if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
     require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
-*/
