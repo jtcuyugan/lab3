458852ecbfa1f60ead1215ee81da1e0fd7706b9d
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-13 13:12:29 +0800

    Trying to CSS

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
-    <title>CodeIgniter Tutorial</title>
+<head>
+    <title>[ SPACE JAM ]</title>
+    <link rel="stylesheet" type="text/css" href = "../css/header.css">
 </head>
 <body>
-
-    <h1><?= esc($title) ?></h1>
+    <div id="navbar" id="top">
+        <div id="pageselector">
+            <center>
+                <ul>
+                    <li id="buttons"><a href="index.php" id="buttons">HOME</a></li>
+                    <li id="buttons"><a href="html/Leo.php" id="buttons">SUN</a></li>
+                    <li id="buttons"><a href="html/Gallery.php" id="logo"><img src="images/wootteogallery.png" id="logo" padding="none"></a></li>
+                    <li id="buttons"><a href="html/Sagi.php" id="buttons">MOON</a></li>
+                    <li id="buttons"><a href="html/Libra.php" id="buttons">RISING</a></li>
+                </ul>
+            </center>
+            <hr id="header">
+        </div>
+    </div>
