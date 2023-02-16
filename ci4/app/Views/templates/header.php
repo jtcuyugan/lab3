e42724e3a2c88c8555bcf70075618268b7977430
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-16 12:39:06 +0800

    CSS Tag Update

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <head>
+    <?php echo link_tag('css/header.css'); ?>
     <title>[ SPACE JAM ]</title>
-    <link rel="stylesheet" type="text/css" href = "<?php echo base_url('css/header.css'); ?>"
+    
 </head>
 <body>
     <div id="navbar" id="top">
