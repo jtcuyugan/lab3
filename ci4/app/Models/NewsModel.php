2bdead6057adae7a48ac2b2b137ca0614e5a88df
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-22 22:43:01 +0800

    News Page Updated {Doesn't work on localhost T^T)

---
+++ ci4/app/Models/NewsModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class NewsModel extends Model
+{
+    protected $table = 'jcuyugan_News';
+    
+    public function getNews($slug = false)
+    {
+        if ($slug === false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug])->first();
+    }
+}
+
