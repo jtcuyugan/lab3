0efda48f85d93d62352e6f16912d7a3f7439c42a
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-27 01:11:30 +0800

    Trial

--- ci4/app/Views/pages/Feedback.php
+++ ci4/app/Views/pages/Feedback.php
@@ -82,7 +82,7 @@
 
 			<p style="font-size: 20; font-family: joane_stencilregular;" id="explore"><a href="Guests" id="explore">Explore the Created Galaxy</a></p>
 
-			<form method="post" action="SQLFeed.php">  
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 			
 			<p style="font-size: 20px; font-family: TimesNewRoman; color: white;" id="restext">Star's Name: <input type="text" name="name" value="<?php echo $name;?>">
 			<span class="error" style="font-size: 20px; font-family: TimesNewRoman; color: white;">* <?php echo $nameErr;?></span></p>
@@ -130,6 +130,43 @@
 			// Close connection
 			mysqli_close($con);
 			}
+			?>
+		<?php
+			if ($_SERVER["REQUEST_METHOD"] == "POST") 
+			{
+				$servername = "192.168.150.213";
+				$username = "webprogmi212";
+				$password = "b3ntRhino98";
+				$dbname = "webprogmi212";
+			
+				// Create connection
+				$con = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+			if($con === false){
+				die("ERROR: Could not connect. "
+					. mysqli_connect_error());
+			}
+
+			// get the post records
+			$name = $_REQUEST['name'];
+			$email = $_REQUEST['email'];
+			$website = $_REQUEST['website'];
+			$comment = $_REQUEST['comment'];
+			$gender = $_REQUEST['gender'];
+
+			// database insert SQL code
+			$sql = "INSERT INTO jcuyugan_Stars VALUES (NULL, '$name', '$email', '$website', '$comment', '$gender', current_timestamp())";
+			
+			if(mysqli_query($con, $sql)){
+				echo "<script> location.href='Feedback'; </script>";
+        		exit;
+			} else{
+				echo "Failed to register star";
+				mysqli_error($con);
+			}
+			// Close connection
+			mysqli_close($con);
+		}
 			?>
 			<br><br><br>
 		</center>
