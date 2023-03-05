323c164fa48dcd9c13d2720d1da71363d8b0f75b
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-03-06 03:32:53 +0800

    Attempt in dividing starpages

--- ci4/app/Views/pages/Leo.php
+++ ci4/app/Views/pages/Leo.php
@@ -8,30 +8,31 @@
 <body>
 
 <br><br><br><br><br>
-<center>
-<div class="container">
-		<img src="images/leo.png" alt="The Leo Constellation" height="600px">
-		<div class="overlay" style="font-size: 30px; font-family: joane_stencilregular;">
-			<div class="text">She is masterful, she likes authority, she aspires toward an ideal. She likes to give advice. She is honest, frank, loyal, open, and sincere.</div>
-		  </div>
+
+<div id="cont">
+	<center>
+	<div class="container">
+			<img class="star" src="images/leo.png" alt="The Leo Constellation" height="600px">
+			<div class="overlay" style="font-size: 30px; font-family: joane_stencilregular;">
+				<div class="text">She is masterful, she likes authority, she aspires toward an ideal. She likes to give advice. She is honest, frank, loyal, open, and sincere.</div>
+			</div>
+	</div>
+	</center>
+	<div id="contents">
+		<br>
+			<p  id="lobbytext" style="font-size: 55px; font-family: joane_stencilregular; color: white; text-align: center;">The Lion</p>
+			<br>
+			<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;" >Name</p>
+			<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;" >REDACTED</p>
+			<br>
+			<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Aliases</p>
+			<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">Jam / Jamzy / dalbitjam</p>
+			<br>
+			<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Age</p>
+			<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">20 Years Old (2022)</p>
+			<br>
+			<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Discovery Date</p>
+			<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">REDACTED</p>
+		<br>
+	</div>
 </div>
-</center>
-<center>
-<div id="contents">
-	<br>
-		<p  id="lobbytext" style="font-size: 55px; font-family: joane_stencilregular; color: white; text-align: center;">The Lion</p>
-		<br>
-		<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;" >Name</p>
-		<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;" >REDACTED</p>
-		<br>
-		<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Aliases</p>
-		<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">Jam / Jamzy / dalbitjam</p>
-		<br>
-		<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Age</p>
-		<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">20 Years Old (2022)</p>
-		<br>
-		<p id="lobbytext" style="font-size: 30px; font-family: joane_stencilregular; color: white; text-align: center;">Discovery Date</p>
-		<p id="lobbytext" style="font-size: 20px; font-family: TimesNewRoman; color: lightgray; text-align: center;">REDACTED</p>
-	<br>
-</div>
-</center>	
